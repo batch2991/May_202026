d1f53fbe8db81f0dc28cf1f10138295ad791dbf1
ooha <oohadevib@gmail.com>
2026-08-03 21:03:39 +0530

    Cartpage is updated with new print statmt

--- src/test/pages/CartPage.ts
+++ src/test/pages/CartPage.ts
@@ -3,5 +3,6 @@ import { Base } from "./Base";
 export class CartPage extends Base {
     async f1() {
         console.log("Hello from CartPage")
+        console.log("Hello from CartPage")
     }
 }
